bb8cb58fdbb6f123ec546c8a9ec4c666f5214e99
essamgouda97 <essamgouda97@hotmail.com>
2022-04-07 23:08:27 -0600

    testing precommit action

--- qoi_python/encode.py
+++ qoi_python/encode.py
@@ -1,8 +1,12 @@
+from __future__ import annotations
+
 import numpy as np
 
-def encode_numpy(path: str, data: np.ndarray,) -> None:
+
+def encode_numpy(path: str, data: np.ndarray) -> None:
     pass
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     rgb = np.random.randint(low=0, high=255, size=(224, 244, 3)).astype(np.uint8)
-    encode_numpy('./test.qoi',rgb)
+    encode_numpy('./test.qoi', rgb)
--- tests/encode_test.py
+++ tests/encode_test.py
@@ -1,8 +1,17 @@
-from qoi_python.encode import encode
+from __future__ import annotations
+
+import numpy as np
 import pytest
 
-@pytest.mark.parametrize("test_data, expected_out", [
-    ("", ""),
-])
+from qoi_python.encode import encode_numpy
+
+
+@pytest.mark.parametrize(
+    'test_data, expected_out', [
+        ('', ''),
+    ],
+)
 def test_encode(test_data, expected_out):
+    rgb = np.random.randint(low=0, high=255, size=(224, 244, 3)).astype(np.uint8)
+    encode_numpy('./test.qoi', rgb)
     assert True
