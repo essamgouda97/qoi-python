27564b9560b5c579a09f32aa43aa0e411c82e0d7
essamgouda97 <essamgouda97@hotmail.com>
2022-04-14 19:22:16 -0600

    decoder finalized

--- qoi_python/decode.py
+++ qoi_python/decode.py
@@ -1,4 +1,3 @@
-#  TODO support RGB and RGBA
 from __future__ import annotations
 
 from struct import unpack
@@ -10,21 +9,35 @@ import numpy as np
 LIST_MAX_SIZE = 64
 
 
-def QOI_OP_INDEX(chunk: int, pixel_list: list[list[int]]) -> list[int]:
-    return pixel_list[chunk]
+def index_position(pixel: list[int]) -> int:
+    return (pixel[0] * 3 + pixel[1] * 5 + pixel[2] * 7 + pixel[3] * 11) % LIST_MAX_SIZE
 
 
-def add_diff(dr: int, dg: int, db: int, prev_pixel: list[int]) -> list[int]:
-    r = (prev_pixel[0] + dr)
-    g = (prev_pixel[1] + dg)
-    b = (prev_pixel[2] + db)
-    a = prev_pixel[3]
-
-    return [r, g, b, a]
+def QOI_OP_RGBA(chunk: int, pixel_list: list[list[int]], prev_pixel: list[int], data_gen: Generator[int, int, str]) -> tuple[int, int, int, int, int]:
+    return (next(data_gen), next(data_gen), next(data_gen), next(data_gen), 1)
 
 
-def QOI_OP_RUN(chunk: Any) -> int:
-    return (chunk & 0x3f) + 1
+def QOI_OP_RGB(chunk: int, pixel_list: list[list[int]], prev_pixel: list[int], data_gen: Generator[int, int, str]) -> tuple[int, int, int, int, int]:
+    return (next(data_gen), next(data_gen), next(data_gen), prev_pixel[3], 1)
+
+
+def QOI_OP_INDEX(chunk: int, pixel_list: list[list[int]], prev_pixel: list[int], data_gen: Generator[int, int, str]) -> tuple[int, int, int, int, int]:
+    pixel = pixel_list[chunk]
+    return (pixel[0], pixel[1], pixel[2], pixel[3], 1)
+
+
+def QOI_OP_DIFF(chunk: int, pixel_list: list[list[int]], prev_pixel: list[int], data_gen: Generator[int, int, str]) -> tuple[int, int, int, int, int]:
+    return ((prev_pixel[0] + ((chunk >> 4) & 0x03) - 2), (prev_pixel[1] + ((chunk >> 2) & 0x03) - 2), (prev_pixel[2] + (chunk & 0x03) - 2), prev_pixel[3], 1)
+
+
+def QOI_OP_LUMA(chunk: int, pixel_list: list[list[int]], prev_pixel: list[int], data_gen: Generator[int, int, str]) -> tuple[int, int, int, int, int]:
+    dg = (chunk & 0x3f) - 32
+    chunk = next(data_gen)
+    return (prev_pixel[0] + ((chunk >> 4) & 0x0f) - 8 + dg, prev_pixel[1] + dg, prev_pixel[2] + (chunk & 0x0f) - 8 + dg, prev_pixel[3], 1)
+
+
+def QOI_OP_RUN(chunk: int, pixel_list: list[list[int]], prev_pixel: list[int], data_gen: Generator[int, int, str]) -> tuple[int, int, int, int, int]:
+    return (prev_pixel[0], prev_pixel[1], prev_pixel[2], prev_pixel[3], (chunk & 0x3f) + 1)
 
 
 def data_gen_func(data: Any) -> Generator[int, int, str]:
@@ -32,19 +45,22 @@ def data_gen_func(data: Any) -> Generator[int, int, str]:
     return 'Done'
 
 
+def is_valid_format(data: bytes) -> bool:
+    magic_bytes = data[:4]  # Get the magic bytes
+    end_bytes = data[-8:]  # Get the end bytes
+
+    return magic_bytes == b'qoif' and end_bytes == b'\x00\x00\x00\x00\x00\x00\x00\x01'
+
+
 def decode(path: str) -> np.ndarray:
     with open(path, 'rb') as f:
         data = f.read()
 
-    magic_bytes = data[:4]  # Get the header bytes
-    if magic_bytes != b'qoif':
-        raise TypeError('Invalid magic bytes')
+    if not is_valid_format(data):
+        raise TypeError('Invalid end bytes')
+
     width, height, channels, colorspace = unpack('>IIBB', data[4:14])
     print(f'{width}:{height}:{channels}:{colorspace}')
-
-    end_bytes = data[-8:]  # Get the end bytes
-    if end_bytes != b'\x00\x00\x00\x00\x00\x00\x00\x01':
-        raise TypeError('Invalid end bytes')
 
     prev_pixel = [0, 0, 0, 255]  # rgba
     data = data[14:-8]
@@ -54,58 +70,32 @@ def decode(path: str) -> np.ndarray:
     height_cnt = 0
     width_cnt = 0
 
+    func_dict = {
+        0xff: QOI_OP_RGBA,
+        0xfe: QOI_OP_RGB,
+        0x00: QOI_OP_INDEX,
+        0x01: QOI_OP_DIFF,
+        0x02: QOI_OP_LUMA,
+        0x03: QOI_OP_RUN,
+    }
+
     while True:
         try:
             chunk = next(data_gen)
         except StopIteration:
             break
-        run = 1
-        if chunk == 255:
-            r = next(data_gen)
-            g = next(data_gen)
-            b = next(data_gen)
-            a = next(data_gen)
-        elif chunk == 254:
-            r = next(data_gen)
-            g = next(data_gen)
-            b = next(data_gen)
-            a = prev_pixel[3]
-        else:
-            tag = (chunk & 0b11000000) >> 6
 
-            if tag == 0:
-                r, g, b, a = QOI_OP_INDEX(chunk, pixel_list)
-            elif tag == 1:
-                dr = ((chunk >> 4) & 0x03) - 2
-                dg = ((chunk >> 2) & 0x03) - 2
-                db = (chunk & 0x03) - 2
-                r, g, b, a = add_diff(dr, dg, db, prev_pixel)
-            elif tag == 2:
-                dg = (chunk & 0x3f) - 32
-                chunk = next(data_gen)
-                dr_dg = ((chunk >> 4) & 0x0f) - 8
-                db_dg = (chunk & 0x0f) - 8
-                dr = dr_dg + dg
-                db = db_dg + dg
-                r, g, b, a = add_diff(dr, dg, db, prev_pixel)
-            elif tag == 3:
-                run = QOI_OP_RUN(chunk)
-                r, g, b, a = prev_pixel
-            else:
-                raise TypeError('Invalid chunk')
-        prev_pixel = [r, g, b, a]
+        func = func_dict[chunk] if chunk == 0xff or chunk == 0xfe else func_dict[(chunk & 0b11000000) >> 6]
+        (*prev_pixel, run) = func(chunk, pixel_list, prev_pixel, data_gen)
+
         pixel_list[index_position(prev_pixel)] = prev_pixel
         for _ in range(run):
-            img_array[height_cnt][width_cnt] = [r, g, b, a]
+            img_array[height_cnt][width_cnt] = prev_pixel
             width_cnt += 1
             if width_cnt == width:
                 width_cnt = 0
                 height_cnt += 1
     return img_array
-
-
-def index_position(pixel: list[int]) -> int:
-    return (pixel[0] * 3 + pixel[1] * 5 + pixel[2] * 7 + pixel[3] * 11) % LIST_MAX_SIZE
 
 
 if __name__ == '__main__':
